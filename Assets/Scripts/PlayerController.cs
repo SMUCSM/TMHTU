2217b428ca026a4c8832c261cdf0ea4fe61521a9
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-03-20 20:16:44 -0300

    first script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public float tilt;
+	public Boundary bounds;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		//Get user input
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		//Apply input to ship
+		Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+		rigidbody.velocity = movement * speed;
+
+		//Lock the ship
+		rigidbody.position = new Vector3
+			(
+				Mathf.Clamp(rigidbody.position.x, bounds.xMin, bounds.xMax),
+				0f,
+				Mathf.Clamp(rigidbody.position.z, bounds.zMin, bounds.zMax)
+			);
+
+		//Tilt
+		rigidbody.rotation = Quaternion.Euler(0f, 0f, rigidbody.velocity.x * -tilt);
+	}
+}
