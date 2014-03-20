94eda1704085eaa109efd3a443a01e740a0db64a
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-03-20 20:34:49 -0300

    bullet movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour 
+{
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		rigidbody.velocity = transform.forward * speed;
+	}
+}
