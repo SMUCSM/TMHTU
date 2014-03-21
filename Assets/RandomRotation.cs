bacbc286a1d443fbf68146e6d2c88053cb1d64bf
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-03-20 21:14:28 -0300

    no rotate?

---
+++ Assets/RandomRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+
+	public float tumbleSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumbleSpeed;
+	}
+
+}
