89ce02b32b3d6e82c6285edcbbaf57cbe5261e43
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-03-20 21:35:43 -0300

    score

---
+++ Assets/GameController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject asteroid;
+	public Vector3 spawnValues;
+
+	public int asteroidCount;
+	public float spawnWaitTime;
+	public float startWaitTime;
+	public float waveWaitTime;
+
+	public GUIText scoreText;
+	private int score;
+
+	// Use this for initialization
+	void Start () 
+	{
+		score = 0;
+		UpdateScore();
+		StartCoroutine(SpawnWaves());
+	}
+
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds(startWaitTime);
+
+		while(true)
+		{
+			for (int i=0; i<asteroidCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate(asteroid, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWaitTime);
+			}
+			yield return new WaitForSeconds(waveWaitTime);
+		}
+
+
+	}
+
+	public void AddScore (int scoreAmount)
+	{
+		score += scoreAmount;
+		UpdateScore();
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
